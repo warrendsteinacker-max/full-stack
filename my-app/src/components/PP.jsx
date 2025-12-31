03147180a284b65e3edb8341fcf484ed20521e2f
Warren <warrendsteinacker@gmail.com>
2025-12-31 16:01:20 -0500

    creating new page

---
+++ my-app/src/components/PP.jsx
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+
+
+
+function PP() {
+    const [state, dispatch] = useReducer(reducer, action)
+
+    
+
+    return (
+        <div>
+            <h1>PP Page</h1>
+        </div>
+    );
+}
