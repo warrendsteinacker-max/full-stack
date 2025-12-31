89627a2e1feba654a4ce60c0854adde03f1e8bdc
Warren <warrendsteinacker@gmail.com>
2025-12-31 16:10:45 -0500

    trying to use the hook

--- my-app/src/components/PP.jsx
+++ my-app/src/components/PP.jsx
@@ -1,11 +1,25 @@
+import { color } from "html2canvas/dist/types/css/types/color";
 import { useReducer } from "react";
 
 
 
 function PP() {
-    const [state, dispatch] = useReducer(reducer, action)
 
+    const s = {
+        count: 0,
+        color: 'red',
+    };
     
+    const [state, dispatch] = useReducer(reducer, s)
+
+        function reducer(state, action)  {
+            switch(action.type){
+                case: IN;
+            }
+        }
+    
+
+
 
     return (
         <div>
